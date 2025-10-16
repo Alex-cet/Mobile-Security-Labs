a464dbda979fc12df0a08e571bd2443720c4e823
alexcetanas3 <alexcetanas3@gmail.com>
2025-10-16 15:25:55 +0300

    Final game state

--- Lab2/app/src/main/java/com/example/lab2/Game.java
+++ Lab2/app/src/main/java/com/example/lab2/Game.java
@@ -1,8 +1,12 @@
 package com.example.lab2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -31,6 +36,7 @@ public class Game extends AppCompatActivity {
             {0, 4, 8},
             {2, 4, 6}
     };
+    String current_player;
     int number_of_moves = 0;
     boolean winner = false;
 
@@ -40,20 +46,62 @@ public class Game extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
 
+        Toolbar toolbar = findViewById (R.id.game_toolbar) ;
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
         Intent intent = getIntent();
         String player1 = intent.getStringExtra(StartGame.player1);
         String player2 = intent.getStringExtra(StartGame.player2);
 
         Random random = new Random();
-        String randomPlayer = random.nextBoolean() ? player1 : player2;
+        current_player = random.nextBoolean() ? player1 : player2;
         TextView game_state_view = findViewById(R.id.game_state);
-        game_state_view.setText("Game State: " + randomPlayer + "'s turn");
+        game_state_view.setText("Game State: " + current_player + "'s turn");
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_resource,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId != R.id.change_background_color_action) {
+            return super.onOptionsItemSelected(item);
+        }
+
+        int initialColor = Color.WHITE;
+
+        ColorPickerDialog colorPickerDialog = new ColorPickerDialog(this, initialColor, new ColorPickerDialog.OnColorSelectedListener() {
+
+            @Override
+            public void onColorSelected(int color) {
+                showToast(color);
+                View mainLayout = findViewById(R.id.main);
+                mainLayout.setBackgroundColor(color);
+            }
+
+        });
+        colorPickerDialog.show();
+
+
+        return true;
+    }
+
+    private void showToast(int color) {
+        String rgbString = "R: " + Color.red(color) + " B: " + Color.blue(color) + " G: " + Color.green(color);
+        Toast.makeText(this, rgbString, Toast.LENGTH_SHORT).show();
     }
 
     public void setXor0(View view) {
@@ -72,14 +120,16 @@ public class Game extends AppCompatActivity {
         if (!winner) {
             if (clickedButton.getText().toString().isEmpty()) {
                 number_of_moves++;
-                if (game_state.contains(player1)) {
+                if (Objects.equals(current_player, player1)) {
                     clickedButton.setText("X");
                     moves[button_pressed_number] = "X";
-                } else if (game_state.contains(player2)) {
+                    current_player = player2;
+                } else if (current_player.equals(player2)) {
                     clickedButton.setText("0");
                     moves[button_pressed_number] = "0";
+                    current_player = player1;
                 }
-                checkGameState(player1, player2, button_pressed_number);
+                checkGameState();
             } else {
                 Toast.makeText(getApplicationContext(),
                         "That spot's already taken!",
@@ -92,9 +142,8 @@ public class Game extends AppCompatActivity {
         }
     }
 
-    public void checkGameState(String player1_name, String player2_name, int button_pressed_number) {
+    public void checkGameState() {
         TextView game_state_view = findViewById(R.id.game_state);
-        String game_state = game_state_view.getText().toString();
 
         for (int i = 0; i < 8; i++) {
             winner = false;
@@ -114,13 +163,8 @@ public class Game extends AppCompatActivity {
                 game_state_view.setText("It's a tie!");
                 winner = true;
             } else {
-                if (game_state.contains(player1_name)) {
-                    game_state_view.setText("Game State: " + player2_name + "'s turn");
-                } else if (game_state.contains(player2_name)) {
-                    game_state_view.setText("Game State: " + player1_name + "'s turn");
-                }
+                game_state_view.setText("Game State: " + current_player + "'s turn");
             }
         }
-
     }
 }
